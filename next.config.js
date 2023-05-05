2535cc138cfc3f7581b11d472ac8bd2c11fec13f
kyu <2066apple@naver.com>
2023-05-05 23:39:57 +0900

    🐛 styled component not found 에러 수정

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
   // reactStrictMode를 켜면 아직 버그가 있는모양? 두번 렌더링 된다.
   // https://stackoverflow.com/questions/71847778/why-my-nextjs-component-is-rendering-twice
   reactStrictMode: false,
-  compiler: {
-    styledComponents: true,
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
