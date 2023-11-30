cceaa7fa1523e810ceb35a9bf103d1fadcdf77bf
jeff.park <jeff.park@machbase.com>
2023-11-30 12:32:19 +0900

    feat: use strictmode

--- next.config.js
+++ next.config.js
@@ -2,17 +2,15 @@ const removeImports = require('next-remove-imports')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode를 켜면 아직 버그가 있는모양? 두번 렌더링 된다.
-  // https://stackoverflow.com/questions/71847778/why-my-nextjs-component-is-rendering-twice
-  reactStrictMode: false,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-    return config
-  },
-}
+    // https://stackoverflow.com/questions/71847778/why-my-nextjs-component-is-rendering-twice
+    // reactStrictMode: false,
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
+};
 
-
-module.exports = removeImports(nextConfig)
+module.exports = removeImports(nextConfig);
