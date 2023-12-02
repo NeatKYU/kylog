55a1d51d244deafae5094cd3444596b3b54637b9
kyu <2066apple@naver.com>
2023-12-02 20:33:57 +0900

    chore: prisma upgrade

--- components/providers/NextThemesProvider.tsx
+++ components/providers/NextThemesProvider.tsx
@@ -7,6 +7,7 @@ type ThemeProviderProps = Parameters<typeof NextThemesProvider>[0];
 /**
  * Your app's theme provider component.
  * 'use client' is essential for next-themes to work with app-dir.
+ * https://github.com/pacocoursey/next-themes/tree/main/examples/with-app-dir
  */
 export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
