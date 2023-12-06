99525901b57fa01031a3af64812b775f64bb8e5e
kyu <2066apple@naver.com>
2023-12-06 13:12:54 +0900

    fix: tanstack query refetchOnWindowFocus false

--- components/providers/TanstackProvider.tsx
+++ components/providers/TanstackProvider.tsx
@@ -1,17 +1,25 @@
-'use client';
+'use client'
 
-import React, { useState } from 'react';
-import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React, { useState } from 'react'
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export default function TanstackProvider({ children }: any) {
-    const [client] = useState(new QueryClient());
+    const [client] = useState(
+        new QueryClient({
+            defaultOptions: {
+                queries: {
+                    refetchOnWindowFocus: false,
+                },
+            },
+        })
+    )
 
     return (
         <QueryClientProvider client={client}>
             <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
-    );
+    )
 }
